be65c475e4a3c052ac4a530af7425e35bdd6b04b
Alberto Larraz Dalmases <alarraz@escoladeltreball.org>
2018-10-23 10:10:16 +0200

    update docker-compose install method

--- debian9-ubuntu17-install-docker.sh
+++ debian9-ubuntu17-install-docker.sh
@@ -19,8 +19,13 @@ sudo apt update
 
 sudo apt install docker-ce -y     
 
-sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+#or install with pip last version
+# apt intall python3-pip -y
+# pip3 install docker-compose
+
 docker-compose --version
 sudo systemctl start docker
 
